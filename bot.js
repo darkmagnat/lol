8c8d99a71f7a671e61fa226b7e83287557c3d6a4
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-20 13:04:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,34 +24,6 @@ const prefix = "&";
 /////////////////////////
 ////////////////////////
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`help|تقديم|inv`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
 client.on('message', msg => {
   if (msg.content === 'باك') {
     msg.reply('**ولكم منور يا عسل**');

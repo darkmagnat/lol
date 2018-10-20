15d427f5a069e849162d0aada8d456c66dca707d
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-20 12:59:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,28 @@
-const convert = require("hh-mm-ss")
-const dateFormat = require('dateformat');
-const pretty = require('pretty-ms');
-const rn = require('random-number');
-const moment = require('moment');
-var Canvas = require('canvas')
-var jimp = require('jimp')
-const prefix = "";
-let done = {};
+const Discord = require('discord.js');
+
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
+const prefix = "&";
+/////////////////////////
+////////////////////////
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -923,8 +939,18 @@ client.on('message', message => {
     };
 });
 
-
-
  
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Diamond Codes`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Sea World`,"http://twitch.tv/S-F")
+client.user.setStatus("online")
+ 
+});
+
  
 client.login(process.env.BOT_TOKEN);

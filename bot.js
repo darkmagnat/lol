041d57c2358ce4136c5f3370529aed20cd6e2e2f
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-22 18:44:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,3 +74,17 @@ message.channel.sendEmbed(Embed).then(msg => {
                               ti[id] = (new Date).getTime()
                                 }
                                 });
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Diamond Codes`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Games Bot`,"http://twitch.tv/S-F")
+client.user.setStatus("online")
+ 
+});
+client.login(process.env.BOT_TOKEN);
